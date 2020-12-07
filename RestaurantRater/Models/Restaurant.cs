b66105f648d783775c83b96b8441e3952b39fa54
Evan Lacy <lacyevan@yahoo.com>
2020-12-07 16:01:20 -0500

    Restaurant properties and DbSet

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        //entities - models stored in database
+        //model - something that goes from controller to view or vice versa
+        //this is a data level class
+        //entity model to respresent the restaurant table that will be populated with objects possessing these properties
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
